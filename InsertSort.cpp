3104cae1ac5a24a54aaf30c7f331463768a05f77
zhouglgh <zhou_303425@163.com>
2017-03-20 05:12:03 -0400

    add shell sort.

--- InsertSort.cpp
+++ InsertSort.cpp
@@ -28,3 +28,30 @@ void InsSort::func(int *array,int size)
 		i++;
 	}
 }
+void InsSort::func_shell(int *array,int size)
+{
+	int bulk=(size-1)/2;
+	while(bulk >= 1)
+	{
+		int i=bulk;
+		while(i<size)
+		{
+			int j=0;
+			while(j<i)
+			{
+				if(array[j] > array[i])
+				{
+					while(j<i)
+					{
+						array[i] ^= array[i-bulk] ^= array[i] ^= array[i-bulk];	
+						j+=bulk;
+					}
+					continue;
+				}
+				j+=bulk;
+			}
+			i+=bulk;
+		}
+		bulk = bulk/2;
+	}
+}
--- InsertSort.h
+++ InsertSort.h
@@ -3,6 +3,7 @@ class InsSort
 {
 	private:
 		void func(int *array, int size);
+		void func_shell(int *array,int size);
 	public:
 		int sort(int *array, int size);
 };
